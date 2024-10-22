ebf7440b40368bea3a1c8876cfea57d5d8e98640
Winlypse <nts3@o2.pl>
2024-10-22 18:26:20 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noAdminLogs/NoAdminLogs.kt
@@ -0,0 +1,14 @@
+package winlyps.noAdminLogs
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoAdminLogs : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
